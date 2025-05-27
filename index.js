13ef88cc6a1b2aa16595c57779ca26918ab60a26
alizeba11323 <alizeba11323@gmail.com>
2025-05-27 18:20:23 +0530

    nodejs first class

---
+++ index.js
@@ -0,0 +1,9 @@
+//const sum = require("./sum");
+//console.log("Sum Of Two value ", sum(1, 2));
+const xyz = require("./sum");
+console.log(xyz);
+// const add = xyz.sum(1, 2);
+// const minus = xyz.substract(1, 2);
+// console.log(add);
+// console.log(minus);
+// console.log(xyz.a);
---
+++ sum.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return b - a;
+}
+//module.exports = sum
+module.exports = {
+  sum,
+  substract,
+};
+
+// module.exports.sum = sum;
+// module.exports.substract = substract;
+//exports
+
+//name exports
+// default exports
